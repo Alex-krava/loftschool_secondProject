9f54c9509d757e4288631ea8e0ca07dd1782b50f
Alexandr Kravchenko <Alexandr Kravchenko>
2016-02-22 00:26:53 +0200

    add checkbox and update catalog

--- app/js/main.js
+++ app/js/main.js
@@ -18,8 +18,14 @@
             .removeClass("content-index_content-list--second")
             .removeClass("content-index_content-list--third")
             .addClass("content-index_content-list--first");
+        
+        clear();
+        $(".catalogs-buttons_first")
+            .removeClass("catalogs-buttons_first")
+            .addClass("catalogs-buttons_first--active");
     });
-    $(".catalogs-buttons_second").on("click", function(){
+    
+    $(".catalogs-buttons_second--active").on("click", function(){
         $(".content-index_content")
             .removeClass()
             .addClass("content-index_content--second");
@@ -28,7 +34,13 @@
             .removeClass("content-index_content-list--first")
             .removeClass("content-index_content-list--third")
             .addClass("content-index_content-list--second");
+            
+        clear();
+        $(".catalogs-buttons_second")
+            .removeClass("catalogs-buttons_second")
+            .addClass("catalogs-buttons_second--active");
     });
+    
     $(".catalogs-buttons_third").on("click", function(){
         $(".content-index_content--second")
             .removeClass()
@@ -38,5 +50,17 @@
             .removeClass("content-index_content-list--second")
             .removeClass("content-index_content-list--first")
             .addClass("content-index_content-list--third");
+    
+        clear();
+        $(".catalogs-buttons_third")
+            .removeClass("catalogs-buttons_third")
+            .addClass("catalogs-buttons_third--active");
     });
+    
+    function clear(){
+        $(".catalogs-buttons_first--active").removeClass("catalogs-buttons_first--active").addClass("catalogs-buttons_first");
+        $(".catalogs-buttons_second--active").removeClass("catalogs-buttons_second--active").addClass("catalogs-buttons_second");
+        $(".catalogs-buttons_third--active").removeClass("catalogs-buttons_third--active").addClass("catalogs-buttons_third");
+    }
+    
 })();
