2720e252f68db9b824d3c3496b841ee8c7c9de1f
Alexandr Kravchenko <Alexandr Kravchenko>
2016-02-24 00:14:22 +0200

    update content

--- app/js/main.js
+++ app/js/main.js
@@ -105,3 +105,32 @@
     });
     
 })();
+
+//columnizer
+(function () {
+    $('.info-text_container').columnize({ columns: 2});
+})();
+
+//slideshow
+(function () {
+    $(".content-index_content-item_photo_small_link").on("click", function(e){
+        e.preventDefault();
+        
+        var $this = $(this),
+            item = $this.closest(".content-index_content-item_photo_small"),
+            container = $this.closest(".content-index_content-item_photo"),
+            display = container.find(".content-index_content-item_photo_big"),
+            path = item.find('img').attr('src'),
+            duration = 300;
+        
+        path = path.substr(0, path.length - 5) + "b.png";
+        
+        if(!item.hasClass("active")){
+            item.addClass('active').siblings().removeClass("active");
+            
+            display.find('img').fadeOut(duration, function(){
+                $(this).attr('src', path).fadeIn(duration);
+            });
+        }
+    });
+})();
