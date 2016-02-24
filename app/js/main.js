4a6225fe6163d5ba564ff796e2bb407fe952210a
Alexandr Kravchenko <Alexandr Kravchenko>
2016-02-24 19:21:43 +0200

    rework js and html

--- app/js/main.js
+++ app/js/main.js
@@ -76,8 +76,30 @@
         $( "#amount-second" ).val(ui.values[1]);
       }
     });
-    $( "#amount-first" ).val( $( "#slider-range" ).slider( "values", 0 ));
+    
+    $( "#amount-first" ).val($( "#slider-range" ).slider( "values", 0 ));
     $( "#amount-second" ).val($( "#slider-range" ).slider( "values", 1 ));
+    
+    $("input#amount-first").change(function () {
+        var value1 = $("input#amount-first").val();
+        var value2 = $("input#amount-second").val();
+            
+            if(parseInt(value1) > parseInt(value2)){
+                value1 = value2;
+                $("input#amount-first").val(value1);
+            }
+            $("#slider-range").slider("values", 0, value1);
+    });
+    $("input#amount-second").change(function () {
+        var value1 = $("input#amount-first").val();
+        var value2 = $("input#amount-second").val();
+            
+            if(parseInt(value1) > parseInt(value2)){
+                value2 = value1;
+                $("input#amount-second").val(value2);                
+            }
+            $("#slider-range").slider("values", 1, value2);
+    });
 })();
 
 //ACCORDION
